96a8a4427302b53bbdb1c7717bf7f1e6d93fbbd8
Andrés Santiago <andresleosan@gmail.com>
2026-04-19 19:07:26 -0500

    chore: Export all molecule components from index.ts with TypeScript types

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,8 +1,17 @@
 // Molecular Component Exports
 // Molecules: Combinations of atoms (ProductCard, CartItem, SearchInput, etc)
 
-// export { ProductCard } from './ProductCard'
-// export { CartItem } from './CartItem'
-// export { SearchInput } from './SearchInput'
-// export { FilterGroup } from './FilterGroup'
-// export { Pagination } from './Pagination'
+export { ProductCard } from './ProductCard'
+export type { ProductCardProps } from './ProductCard'
+
+export { CartItem } from './CartItem'
+export type { CartItemProps } from './CartItem'
+
+export { SearchInput } from './SearchInput'
+export type { SearchInputProps } from './SearchInput'
+
+export { FilterGroup } from './FilterGroup'
+export type { FilterGroupProps } from './FilterGroup'
+
+export { Pagination } from './Pagination'
+export type { PaginationProps } from './Pagination'
