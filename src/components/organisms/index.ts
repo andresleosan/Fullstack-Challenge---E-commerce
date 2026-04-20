86b31df2b7971b5dbc03019d908095b152e36459
Andrés Santiago <andresleosan@gmail.com>
2026-04-19 19:24:40 -0500

    chore: Export all organism components from index.ts with TypeScript types

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,8 +1,17 @@
 // Organism Component Exports
-// Organisms: Large, complex components (Header, Footer, ProductGallery, CartSidebar, MainLayout, etc)
+// Organisms: Complex combinations of molecules and atoms (Header, Footer, ProductGallery, etc)
 
-// export { Header } from './Header'
-// export { Footer } from './Footer'
-// export { MainLayout } from './MainLayout'
-// export { ProductGallery } from './ProductGallery'
-// export { CartSidebar } from './CartSidebar'
+export { Header } from './Header'
+export type { HeaderProps } from './Header'
+
+export { Footer } from './Footer'
+export type { FooterProps } from './Footer'
+
+export { ProductGallery } from './ProductGallery'
+export type { ProductGalleryProps } from './ProductGallery'
+
+export { CartSidebar } from './CartSidebar'
+export type { CartSidebarProps } from './CartSidebar'
+
+export { MainLayout } from './MainLayout'
+export type { MainLayoutProps } from './MainLayout'
