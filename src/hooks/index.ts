0b6a2315a7cf271c9f2a74984627ed185cf4c1f5
Andrés Santiago <andresleosan@gmail.com>
2026-04-19 19:39:18 -0500

    chore: Export all custom hooks from index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,11 @@
 // Custom Hooks Exports
 // Hooks: React hooks for shared logic (useCart, useUser, useProducts, useDebounce, useNotification)
 
-// export { useCart } from './useCart'
-// export { useUser } from './useUser'
-// export { useProducts } from './useProducts'
-// export { useDebounce } from './useDebounce'
-// export { useNotification } from './useNotification'
-// export { useLocalStorage } from './useLocalStorage'
+export { useCart } from './useCart'
+export { useUser } from './useUser'
+export { useProducts } from './useProducts'
+export { useDebounce } from './useDebounce'
+export { useNotification } from './useNotification'
+export type { Notification } from './useNotification'
 
 // Individual exports will be added as hooks are created
